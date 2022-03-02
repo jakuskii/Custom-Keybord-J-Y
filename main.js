3c0330b1205cb900b79691125262489212b52e78
jakuskii <74100491+jakuskii@users.noreply.github.com>
2022-03-02 20:41:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,20 +22,9 @@ faqs.forEach(faq =>{
 })
 
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    //for huge device
-    breakpoints:{
-        600:{
-            slidesPerView: 2,
-        }
-    }
-  });
+
+
+
 
 /*mobile nav*/
 const menu = document.querySelector(".nav__menu");
