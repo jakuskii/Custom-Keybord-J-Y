841a9bf9e22722493a0adb9a0165f4f70f47925c
jakuski <del910815@gmail.com>
2022-03-02 19:31:38 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+//navi bari
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
+})
+//faq hide
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq =>{
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+        const icon=faq.querySelector('.faq__icon i');
+        if(icon.className ==='uil uil-plus'){
+            icon.className = "uil uil-minus";
+      }else{
+          icon.className="uil uil-plus"
+      }
+
+
+    } )
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    //for huge device
+    breakpoints:{
+        600:{
+            slidesPerView: 2,
+        }
+    }
+  });
+
+/*mobile nav*/
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',() =>{
+    menu.style.display="flex";
+    closeBtn.style.display ="inline-block";
+    menuBtn.style.display="none"
+
+})
+
+const closeNav= () =>{
+    menu.style.display="none";
+    closeBtn.style.display ="none";
+    menuBtn.style.display="inline-block"
+}
+closeBtn.addEventListener ('click', closeNav)
